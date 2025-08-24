aa6bea3036c343005192cc32477cc837459b21c1
Eleonoraraf <133599206+Eleonoraraf@users.noreply.github.com>
2025-08-24 21:44:17 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {
-  keyframes: {
-    fall: {
-      '0%':   { transform: 'translateY(-20%)', opacity: '0' },
-      '10%':  { opacity: '1' },
-      '100%': { transform: 'translateY(110%)', opacity: '1' },
+  theme: {
+    extend: {
+      keyframes: {
+        coinFallOnly: {
+          "0%":   { transform: "translateY(-20vh)" },
+          "100%": { transform: "translateY(120vh)" },
+        },
+      },
+      animation: {
+        fall: "coinFallOnly 7s linear infinite",
+      },
     },
   },
-  animation: {
-    fall: 'fall 6s linear infinite',
-  },
-} },
+  safelist: ["animate-fall"], // пусть останется, не мешает
   plugins: [],
 };
