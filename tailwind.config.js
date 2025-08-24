5806db7e012637dd61d3e3448c75c131f3f37037
Eleonoraraf <133599206+Eleonoraraf@users.noreply.github.com>
2025-08-24 21:03:30 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: { extend: {
+  keyframes: {
+    fall: {
+      '0%':   { transform: 'translateY(-20%)', opacity: '0' },
+      '10%':  { opacity: '1' },
+      '100%': { transform: 'translateY(110%)', opacity: '1' },
+    },
+  },
+  animation: {
+    fall: 'fall 6s linear infinite',
+  },
+} },
   plugins: [],
 };
